7ceda4637e77a96b648e3b4e0fb31a545111465f
AurelieTorre <aurelie.torre@laposte.net>
2024-08-06 16:55:52 +0200

    homepage sliders

--- src/components/carrousel/carrousel.jsx
+++ src/components/carrousel/carrousel.jsx
@@ -1,10 +1,33 @@
+import React, { useState } from 'react';
 import s from "./style.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-const Slider = () => {
+
+const Slider = ({ images }) => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
 
   return (
-    <div className="carrousel">
-
+    <div className={s.carrousel}>
+      <button className={`${s.arrow} ${s.left}`} onClick={prevImage}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <div className={s.imageContainer}>
+        <img src={images[currentIndex]} alt="Carrousel" className={s.image} />
+      </div>
+      <button className={`${s.arrow} ${s.right}`} onClick={nextImage}>
+        <FontAwesomeIcon icon={faChevronRight} />
+      </button>
     </div>
   );
 };
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,19 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Slider from '../components/carrousel/Carrousel.jsx';
+import cabane from "../assets/cabane-soizen.jpg";
+import zip from "../assets/fermeture.jpg";
+import feu from "../assets/feu.jpg";
+import glycine from "../assets/hanvec.jpg";
+import plage from "../assets/plage-steir.jpg";
+import trou from "../assets/trou.jpg";
+import veranda from "../assets/veranda.jpg";
+import alain from "../assets/alainhergott.jpg";
+import botton from "../assets/botton.jpg";
+import chabaud from "../assets/cathchab.jpg";
+import jc from "../assets/jclecloarec.jpg";
+
+
+const imagesFresk = [
+  cabane,
+  zip,
+  feu,
+  glycine,
+  plage,
+  trou,
+  veranda
+];
+
+const imagesFaces = [
+  alain,
+  botton,
+  chabaud,
+  jc
+]
 
 const Home = () => {
 
   return (
     <div>
       <div className="content">
+        <h2>Trompe l’œil en fresques</h2>
         {/* section trompe-l'œil */}
-        <Slider />
+        <Slider images={imagesFresk} />
         {/* détail */}
         {/* bouton */}
       </div>
       <div className="content">
-        {/* section portraits */}
-        <Slider />
+        <h2>Portraits chinois</h2>
+        <Slider images={imagesFaces} />
         {/* détail */}
         {/* bouton */}
       </div>
