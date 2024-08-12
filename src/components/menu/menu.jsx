7f34b15e9196a3c09ce758fc9b0c3b6672d23220
AurelieTorre <aurelie.torre@laposte.net>
2024-08-12 18:59:13 +0200

    about ok

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from "./style.module.css";
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
@@ -11,14 +11,27 @@ const Menu = ({ onDevisClick }) => {
   // N'affiche pas le devis si on est sur les mentions légales
   const showDevisLink = location.pathname !== '/mentions';
 
+  // Pour gérer le clic
+  const [activeItem, setActiveItem] = useState('');
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+
   return (
     <div className={s.navbar}>
       <Link to="/"><img src={logo} alt="Logo" id={s.vague} /></Link>
-      <Link to="/trompe-loeil">Peinture en décor</Link>
-      <Link to="/portraits">Portraits chinois</Link>
-      <Link to="/oeuvres">Autres œuvres</Link>
-      <Link to="/lartiste">À propos</Link>
-      {showDevisLink && <a href="#" onClick={(e) => { e.preventDefault(); onDevisClick(); }}>Devis</a>}
+      <Link to="/trompe-loeil">
+        <span
+          className={`${s.item} ${activeItem === 'trompe-loeil' ? s.active : ''}`}
+          onClick={() => handleItemClick('trompe-loeil')}
+        >
+          Peinture en décor
+        </span>
+      </Link>
+      <Link to="/portraits"><span className={s.item}>Portraits chinois</span></Link>
+      <Link to="/oeuvres"><span className={s.item}>Autres œuvres</span></Link>
+      <Link to="/lartiste"><span className={s.item}>À propos</span></Link>
+      {showDevisLink && <a href="#" onClick={(e) => { e.preventDefault(); onDevisClick(); }}><span className={s.item}>Devis</span></a>}
     </div>
   );
 };
