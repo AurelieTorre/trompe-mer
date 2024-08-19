54e07638a84d44a93e85610f63ade59c4a5a1bda
AurelieTorre <aurelie.torre@laposte.net>
2024-08-19 19:57:52 +0200

    legal page css ok

--- src/pages/legal.jsx
+++ src/pages/legal.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Legal = () => {
 
   return (
-    <div className="content">
+    <div className="legal-content">
       <div className="mentions">
         <h2>Mentions légales</h2>
-        <p>En vigueur au 27/08/2024</p>
+        <p id="vigueur">En vigueur au 27/08/2024</p>
         <p>Conformément aux dispositions de la loi n°2004-575 du 21 juin 2004 pour la Confiance en l’économie numérique, il est porté à la connaissance des utilisateurs et visiteurs, ci-après l’“Utilisatrice”, du site “www.trompeloeilmer.torre.bzh”, ci-après “le Site”, les présentes mentions légales.</p>
         <p>La connexion et la navigation sur le Site par l’Utilisatrice implique acceptation intégrale et sans réserve des présentes mentions légales.</p>
         <p>Ces dernières sont accessibles sur le Site à la rubrique “Mentions légales”.</p>
@@ -14,32 +14,34 @@ const Legal = () => {
       <div className="edition">
         <h2>Édition du Site</h2>
         <p>L’édition et la direction de la publication du Site est assurée par l’Agence web Lilitor, domiciliée 2A allée Jacques Frimot – 35000 Rennes, dont le numéro de téléphone est 0612345678, et l’adresse email contact@lilitor.com, ci-après l’“Éditrice”.</p>
-        <p>Hébergement :</p>
+        <p className='alinea'>Hébergement :</p>
         <p>L'hébergeur du Site est la société OVH, dont le siège social est situé au 2 rue Kellermann – 59100 Roubaix. Le numéro de téléphone de l’hébergeur est le 1007</p>
-        <p>Accès au Site :</p>
+        <p className='alinea'>Accès au Site :</p>
         <p>Le Site est normalement accessible, à tout moment, à l’Utilisatrice. Toutefois, l’Éditrice pourra, à tout moment, suspendre, limiter ou interrompre le Site afin de procéder, notamment, à des mises à jour ou des modifications de son contenu. L’Éditrice ne pourra en aucun cas être tenu responsable des conséquences éventuelles de cette indisponibilité sur les activités de l’Utilisatrice.</p>
-        <p>“Trompe-l’œil de mer” appartient à :</p>
+        <p className='alinea'>“Trompe-l’œil de mer” appartient à :</p>
         <p>Virginie Théry<br />lieu dit Loc’h ar Raor<br />29140 Saint-Yvi</p>
         <p>Artiste-auteur – SIRET n° ...</p>
       </div>
       <div className="edition">
         <h2>Utilisation des données personnelles collectées</h2>
-        <p>Législation :</p>
+        <p className='alinea'>Législation :</p>
         <p>Nous nous engageons à respecter les dispositions législatives énoncées dans le règlement général sur la protection des données (RGPD).</p>
-        <p>Les informations que nous recueillons :</p>
-        <p>Nous ne recueillons aucune information par cookie.Cependant, via le formulaire de contact, vous pouvez faire des demandes et envoyer des documents.Les types d'informations personnelles collectées sur ce formulaire sont du type suivant :</p>
+        <p className='alinea'>Les informations que nous recueillons :</p>
+        <p>Nous ne recueillons aucune information par cookie.Cependant, via le formulaire de contact, vous pouvez faire des demandes et envoyer des documents.</p>
+        <p>Les types d'informations personnelles collectées sur ce formulaire sont du type suivant :</p>
         <ul>
           <li>Nom et prénom</li>
           <li>Adresse</li>
           <li>Numéro de téléphone</li>
           <li>Plan d’une pièce</li>
         </ul>
-        <p>Les informations recueillies servent exclusivement à vous recontacter à la suite de votre message. Elles sont gardées de manière sécurisée et sont effacées au bout d’un an sans action de votre part.</p>
+        <p className='alinea'>Les informations recueillies servent exclusivement à vous recontacter à la suite de votre message. Elles sont gardées de manière sécurisée et sont effacées au bout d’un an sans action de votre part.</p>
         <p>Le Site assure à l'Utilisateur une collecte et un traitement des données personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers aux libertés et dans le respect de la règlementation applicable en matière de traitement des données à caractère personnel conformément au règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016 (ci-après, ensemble, la "Règlementation applicable en matière de protection des Données à caractère personnel").</p>
+        <p className='alinea'>Droit à l’effacement (“droit à l'oubli”) :</p>
         <p>En vertu de la Règlementation applicable en matière de protection des Données à caractère personnel, l'Utilisatrice dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. Vous pouvez exercer ce droit par mail à l'adresse : support@trompeloeilmer.torre.bzh.</p>
-        <p>Autorisations supplémentaires :</p>
+        <p className='alinea'>Autorisations supplémentaires :</p>
         <p>Si l’artiste souhaite exposer une réalisation faite à votre domicile, des documents encadrant strictement cette utilisation et l’anonymisation vous seront soumis. Tout refus de votre part sera absolument respecté.</p>
-        <p>Droit d’auteur :</p>
+        <p className='alinea'>Droit d’auteur :</p>
         <p>Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans autorisation expresse de l’'Éditrice est prohibée et pourra entraîner des actions et poursuites judiciaires telles que prévues par la règlementation en vigueur.</p>
       </div>
     </div>
