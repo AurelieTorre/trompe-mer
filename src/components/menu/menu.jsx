cd8bac75153b4a9fb74b29a2e2ed59a76d536c3b
AurelieTorre <aurelie.torre@laposte.net>
2024-08-28 09:56:05 +0200

    menu burger ok

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -11,27 +11,44 @@ const Menu = ({ onDevisClick }) => {
   // N'affiche pas le devis si on est sur les mentions légales
   const showDevisLink = location.pathname !== '/mentions';
 
-  // Pour gérer le clic
+  // Pour gérer le clic sur les éléments du menu
   const [activeItem, setActiveItem] = useState('');
   const handleItemClick = (itemName) => {
     setActiveItem(itemName);
   };
 
+  // Pour gérer l'ouverture et la fermeture du menu burger
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className={s.navbar}>
       <Link to="/"><img src={logo} alt="Logo" id={s.vague} /></Link>
-      <Link to="/trompe-loeil">
-        <span
-          className={`${s.item} ${activeItem === 'trompe-loeil' ? s.active : ''}`}
-          onClick={() => handleItemClick('trompe-loeil')}
-        >
-          Peinture en décor
-        </span>
-      </Link>
-      <Link to="/portraits"><span className={s.item}>Portraits chinois</span></Link>
-      <Link to="/oeuvres"><span className={s.item}>Autres œuvres</span></Link>
-      <Link to="/lartiste"><span className={s.item}>À propos</span></Link>
-      {showDevisLink && <a href="#" onClick={(e) => { e.preventDefault(); onDevisClick(); }}><span className={s.item}>Devis</span></a>}
+
+      {/* Icône du menu burger */}
+      <div className={s.burgerIcon} onClick={toggleMenu}>
+        <span className={s.burgerLine}></span>
+        <span className={s.burgerLine}></span>
+        <span className={s.burgerLine}></span>
+      </div>
+
+      {/* Menu déroulant */}
+      <div className={`${s.menuItems} ${menuOpen ? s.open : ''}`}>
+        <Link to="/trompe-loeil">
+          <span
+            className={`${s.item} ${activeItem === 'trompe-loeil' ? s.active : ''}`}
+            onClick={() => handleItemClick('trompe-loeil')}
+          >
+            Peinture en décor
+          </span>
+        </Link>
+        <Link to="/portraits"><span className={s.item}>Portraits chinois</span></Link>
+        <Link to="/oeuvres"><span className={s.item}>Autres œuvres</span></Link>
+        <Link to="/lartiste"><span className={s.item}>À propos</span></Link>
+        {showDevisLink && <a href="#" onClick={(e) => { e.preventDefault(); onDevisClick(); }}><span className={s.item}>Devis</span></a>}
+      </div>
     </div>
   );
 };
